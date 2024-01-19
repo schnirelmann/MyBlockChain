98f65315bb0142b39941d97698e10eadc5807f0f
schnirelmann <47248080+schnirelmann@users.noreply.github.com>
2024-01-19 19:33:25 +0100

    tests for block addition and prettier file (formating) added

--- block_chain.js
+++ block_chain.js
@@ -13,7 +13,7 @@ class BlockChain {
     } 
 
     isValidChain(chain) {
-        if (JSON.stringify(chain[0] !== JSON.stringify( Block.genesis() ))) return false;
+        if ( JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis()) ) return false;
 
         for (let i = 1; i < chain.length; i++) {
             const block = chain[i];
@@ -21,11 +21,25 @@ class BlockChain {
 
             if (block.lastHash !== lastBlock.hash || 
                 block.hash !== Block.blockHash(block)) {
-                    return false;
-                }
+                return false;
+            }
         }
 
         return true;
+    }
+
+    replaceChain(newChain) {
+        if (newChain.length <= this.chain.length) {
+            console.log('The length of the new chain is not longer than the current chain');
+            return;
+        } else if (!this.isValidChain(newChain)) {
+            console.log('The new chain is not valid');
+            return;
+        }
+
+        console.log('The new chain is longer and valid. We replace the current chain with the new chain');
+        this.chain = newChain;
+
     }
 }
 
--- block_chain.test.js
+++ block_chain.test.js
@@ -2,10 +2,11 @@ const BlockChain = require('./block_chain');
 const Block = require('./block');
 
 describe('BlockChain', () => { 
-    let bc;
+    let bc, bc2;
 
     beforeEach(() => {
         bc = new BlockChain();
+        bc2 = new BlockChain();
     });
 
     it('Begins with the genesis block', () => {
@@ -13,9 +14,26 @@ describe('BlockChain', () => {
     });
 
     it('Adds a new block', () => {
-        const data = 'gooc data';
+        const data = 'random data';
         bc.addBlock(data);
 
         expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+    });
+
+    it('Checks validity of a chain', () => {
+        bc2.addBlock('Some Data');
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
+    });
+
+    it('Checks validity genesis block', () => {
+        bc2.chain[0].data = 'Faulty Data';
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
+    });
+
+    it('Checks if a chain has a corrupt block', () => {
+        bc2.addBlock('Some Data');
+        bc2.chain[1].data = 'Corrupt Data';
+
+        expect(bc.isValidChain(bc2.chain)).toBe(false);
     });
 });
