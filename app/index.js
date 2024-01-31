bcf21c3e12752ef8dc424c90944ba7ab5b037282
schnirelmann <47248080+schnirelmann@users.noreply.github.com>
2024-01-31 23:03:46 +0100

    transactions added to app and tested with postMan

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require("express");
 const BodyParser = require("body-parser");
 const BlockChain = require("../block_chain");
 const P2pServer = require("./p2p-server");
+const Wallet = require("../wallet");
+const TransactionsPool = require("../wallet/transaction-pool");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new BlockChain();
+const wallet = new Wallet();
+const transactionsPool = new TransactionsPool();
 const p2pServer = new P2pServer(bc);
 
 app.use(BodyParser.json());
@@ -22,6 +26,20 @@ app.post("/mine", (req, res) => {
   p2pServer.synchronizeChains();
 
   res.redirect("/blocks");
+});
+
+app.get("/transactions", (req, res) => {
+  res.json(transactionsPool.transactions);
+});
+
+app.post("/transact", (req, res) => {
+  const { recipient, amount } = req.body;
+  const transaction = wallet.createTransaction(
+    recipient,
+    amount,
+    transactionsPool
+  );
+  res.redirect("/transactions");
 });
 
 app.listen(HTTP_PORT, console.log(`Listening on port ${HTTP_PORT}`));
